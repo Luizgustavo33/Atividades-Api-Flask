c5b9092ea369c7d6ab2088505a2658106cc32cb5
Luiz Gustavo <74794415+Luizgustavo33@users.noreply.github.com>
2022-11-10 22:41:41 -0300

    Atividade 22 e 23

---
+++ F16/estoque.py
@@ -0,0 +1,245 @@
+from flask import Flask
+from flask_restx import reqparse, abort, Api, Resource, fields
+app = Flask(__name__)   
+api = Api(app)
+
+Produtos = [{'id': 0, 'nome': 'calca','quantidade':12, 'preco': 89.94},
+{'id': 1, 'nome': 'camisa','quantidade':54, 'preco': 49.99},
+{'id': 2, 'nome': 'saia','quantidade':33,'preco': 72.14},
+{'id': 3, 'nome': 'sapato','quantidade':12, 'preco': 99.11},
+{'id': 4, 'nome': 'vestido','quantidade':47, 'preco': 78.32}]
+
+def aborta_se_o_produto_nao_existe(id):
+    encontrei = False
+    for produto in Produtos:
+
+        if produto['id'] == int(id):
+            encontrei = True
+    if encontrei == False:
+        abort(404, mensagem="O produto com o código = {} não existe".format(id)) 
+    #404:Not Found
+
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=int, help='Número do código do produto')
+parser.add_argument('nome', type=str, help='Nome do produto')
+parser.add_argument('quantidade', type=float, help='Quantidade de produtos ')
+parser.add_argument('preco', type=float, help='preco do produto')
+parser.add_argument('produtos_vendidos', type=int, help='quantidade de produtos vendidos')
+parser.add_argument('produtos_comprados', type=int, help='quantidade de produtos comprados')
+
+
+
+campos_obrigatorios_para_atualizacao = api.model('Atualizaçao dos produtos', {
+  'id': fields.Integer(required=True, description='identificador dos produtos'),
+  'nome': fields.String(required=True, description='nome do produto'),
+  'quantidade': fields.Integer(required=True, description='quantidade de produtos'),
+  'preco': fields.Float(required=True, description='preco do produto'),
+})
+campos_obrigatorios_para_insercao = api.model('Inserção de produtos', {
+  'id': fields.Integer(required=False, readonly=True,
+description='identificador dos produtos'),
+  'nome': fields.String(required=True, description='nome do produto'),
+  'quantidade': fields.Integer(required=True, description='quantidade de produtos'),
+  'preco': fields.Float(required=True, description='preco do produto'),
+})
+campos_obrigatorios_para_vendas_e_compras = api.model('Vendas de produtos', {
+'produtos_vendidos': fields.Integer(required=True,
+description='quantidade de produtos vendidos')
+})
+
+campos_obrigatorios_para_vendas_e_compras = api.model('Compra de produtos', {
+'produtos_comprados': fields.Integer(required=True,
+description='quantidade de produtos comprados')})
+
+class produto(Resource):
+    
+ 
+    def get(self, id):
+        aborta_se_o_produto_nao_existe(id)
+        return Produtos[int(id)]
+    
+    def delete(self, id):
+        aborta_se_o_produto_nao_existe(id)
+        del Produtos[int(id)]
+        return '', 204, #204: No Content
+    
+
+    
+    @api.doc(responses={200: 'produto substituído'}) #200: OK
+    @api.expect(campos_obrigatorios_para_atualizacao)
+    def put(self, id):
+        aborta_se_o_produto_nao_existe(id)
+        args = parser.parse_args()
+        for produto in Produtos:
+            if produto['id'] == int(id):
+                produto['id'] == int(id)
+                produto['nome'] = args['nome']
+                produto['quantidade'] = args['quantidade']
+                produto['preco'] = args['preco']
+                break
+        return produto, 200, #200: OK
+    
+    @api.doc(responses={200: 'produto substituído'}) #200: OK
+    @api.expect(campos_obrigatorios_para_atualizacao)
+    def patch(self, id):
+        aborta_se_o_produto_nao_existe(id)
+        args = parser.parse_args()
+        for produto in Produtos:
+            if produto['id'] == int(id):
+                produto['quantidade'] = args['quantidade']
+                produto['preco'] = args['preco']
+                break
+        return produto, 200, #200: OK
+
+
+
+class ListaProdutos(Resource):
+    
+    #http://127.0.0.1:5000/produto
+    def get(self):
+        return Produtos
+    
+    
+    #http://127.0.0.1:5000/produto
+    #{"nome":"Carla","preco":45.8}
+    @api.doc(responses={201: 'produto inserido'}) #201: Created
+    @api.expect(campos_obrigatorios_para_insercao)  
+    def post(self):
+        args = parser.parse_args()
+        id = -1
+        for produto in Produtos:
+            if int(produto['id']) > id:
+                    id = int(produto['id'])
+        id = id + 1
+        produto = {'id': id, 'nome': args['nome'],  'quantidade': args['quantidade'], 'preco': args['preco']}
+        Produtos.append(produto)
+        return produto, 201, #201: Created
+    
+class ListaEstoque(Resource):
+    def get(self):
+        estoque = []
+        for produto in Produtos:
+            produto = {'id': produto['id'], 'nome': produto['nome'], 'quantidade' : produto['quantidade'],'preco':produto['preco'], 'total_estoque': produto['quantidade']*produto['preco']}
+            estoque.append(produto)
+        return estoque, 201, #201: Created
+    
+class Estoque(Resource):
+    def get(self, id):
+        aborta_se_o_produto_nao_existe(id)
+        estoque = []
+        produto =  Produtos[int(id)]
+        produto1 = {'id': produto['id'], 'nome': produto['nome'], 'quantidade' : produto['quantidade'],'preco':produto['preco'], 'total_estoque': produto['quantidade']*produto['preco']}
+        estoque.append(produto1)
+        
+        return estoque
+
+class EstoqueMaior(Resource):
+    def get(self):
+        estoque = []
+        
+        for produto in Produtos:
+           
+            produto =  float(produto['quantidade'])
+            estoque.append(produto)
+            
+        maior = float()
+        
+        for i in estoque:
+            if i > maior:
+                maior = i
+        for i in range(len(Produtos)):
+            if maior == Produtos[i]['quantidade']:
+                maior = Produtos[i]
+            else:
+                continue
+        
+        return maior
+    
+class EstoqueMenor(Resource):
+    def get(self):
+        estoque = []
+        
+        for produto in Produtos:
+           
+            produto =  int(produto['quantidade'])
+            estoque.append(produto)
+            
+        menor = estoque[0]
+        
+        for i in estoque:
+            if menor >= i:
+                menor = i
+        
+        for i in range(len(Produtos)):
+            if menor == Produtos[i]['quantidade']:
+                menor = Produtos[i]
+            else:
+                continue
+                
+            
+
+        
+        return menor
+class EstoqueTotal(Resource):
+    def get(self):
+        estoque = []
+        
+        for produto in Produtos:
+           
+            produto =  float(produto['quantidade']*produto['preco'])
+            estoque.append(produto)
+            
+        total = 0
+        for i in estoque:
+            total = total + i
+                
+            
+        total= {'valor_total_estoque': total}
+        return total
+    
+class Venda(Resource):
+    @api.expect(campos_obrigatorios_para_vendas_e_compras)
+
+    def patch(self, id):    
+        aborta_se_o_produto_nao_existe(id)
+        args = parser.parse_args()
+        for produto in Produtos:
+            if produto['id'] == int(id):
+                produto['quantidade'] = int(produto['quantidade']-args['produtos_vendidos'])
+                break
+        return produto, 200, #200: OK
+        
+class Compra(Resource):
+    @api.expect(campos_obrigatorios_para_vendas_e_compras)
+    def patch(self, id):
+        aborta_se_o_produto_nao_existe(id)
+        args = parser.parse_args()
+        for produto in Produtos:
+            if produto['id'] == int(id):
+                produto['quantidade'] = int(produto['quantidade']+args['produtos_comprados'])
+                break
+        return produto, 200, #200: OK
+      
+
+    
+##
+## Roteamento de recursos:
+##  
+api.add_resource(produto, '/produto/<id>')
+api.add_resource(ListaProdutos, '/produto')
+api.add_resource(ListaEstoque, '/estoque')
+api.add_resource(Estoque, '/estoque/<id>')
+api.add_resource(EstoqueMaior, '/estoque/maior')
+api.add_resource(EstoqueMenor, '/estoque/menor')
+api.add_resource(EstoqueTotal, '/estoque/total')
+api.add_resource(Venda, '/venda/<id>')
+api.add_resource(Compra, '/compra/<id>')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ F16/pagamento.py
@@ -0,0 +1,190 @@
+from flask import Flask
+from flask_restx import reqparse, abort, Api, Resource, fields
+app = Flask(__name__)   
+api = Api(app)
+
+Funcionarios = [{'id': 0, 'nome': 'Ana','horas':8, 'valor': 45.78},
+{'id': 1, 'nome': 'Bruna','horas':2, 'valor': 60.0},
+{'id': 2, 'nome': 'Carlos','horas':10,'valor': 38.99},
+{'id': 3, 'nome': 'Diego','horas':4, 'valor': 45.78},
+{'id': 4, 'nome': 'Ester','horas':5, 'valor': 45.78}]
+
+def aborta_se_o_funcionário_nao_existe(id):
+    encontrei = False
+    for funcionario in Funcionarios:
+
+        if funcionario['id'] == int(id):
+            encontrei = True
+    if encontrei == False:
+        abort(404, mensagem="O funcionario com o  CPF = {} não existe".format(id)) 
+    #404:Not Found
+
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=int, help='Número do CPF do funcionario')
+parser.add_argument('nome', type=str, help='Nome do funcionario')
+parser.add_argument('horas', type=float, help='Quantidade de horas trabalhadas')
+parser.add_argument('valor', type=float, help='valor da hora do funcionario')
+
+campos_obrigatorios_para_atualizacao = api.model('Atualizaçao de funcionários', {
+  'id': fields.Integer(required=True, description='identificador do funcionários'),
+  'nome': fields.String(required=True, description='nome do funcionários'),
+  'horas': fields.Integer(required=True, description='quantidade de horas trabalhadas'),
+  'valor': fields.Float(required=True, description='valor da hora de trabalho do funcionário'),
+})
+campos_obrigatorios_para_insercao = api.model('Inserção de funcionários', {
+  'id': fields.Integer(required=False, readonly=True,
+description='identificador do funcionários'),
+  'nome': fields.String(required=True, description='nome do funcionários'),
+  'horas': fields.Integer(required=True, description='quantidade de horas trabalhadas'),
+  'valor': fields.Float(required=True, description='valor da hora de trabalho do funcionário'),
+})
+
+class funcionario(Resource):
+    
+ 
+    def get(self, id):
+        aborta_se_o_funcionário_nao_existe(id)
+        return Funcionarios[int(id)]
+    
+    def delete(self, id):
+        aborta_se_o_funcionário_nao_existe(id)
+        del Funcionarios[int(id)]
+        return '', 204, #204: No Content
+    
+
+    
+    @api.doc(responses={200: 'funcionário substituído'}) #200: OK
+    @api.expect(campos_obrigatorios_para_atualizacao)
+    def put(self, id):
+        aborta_se_o_funcionário_nao_existe(id)
+        args = parser.parse_args()
+        for funcionario in Funcionarios:
+            if funcionario['id'] == int(id):
+                funcionario['id'] == int(id)
+                funcionario['nome'] = args['nome']
+                funcionario['horas'] = args['horas']
+                funcionario['valor'] = args['valor']
+                break
+        return funcionario, 200, #200: OK
+    
+    @api.doc(responses={200: 'funcionário substituído'}) #200: OK
+    @api.expect(campos_obrigatorios_para_atualizacao)
+    def patch(self, id):
+        aborta_se_o_funcionário_nao_existe(id)
+        args = parser.parse_args()
+        for funcionario in Funcionarios:
+            if funcionario['id'] == int(id):
+                funcionario['id'] == int(id)
+                funcionario['horas'] = args['horas']
+                funcionario['valor'] = args['valor']
+                break
+        return funcionario, 200, #200: OK
+
+
+
+class ListaFuncionarios(Resource):
+    
+    #http://127.0.0.1:5000/funcionario
+    def get(self):
+        return Funcionarios
+    
+    
+    #http://127.0.0.1:5000/funcionario
+    #{"nome":"Carla","valor":45.8}
+    @api.doc(responses={201: 'funcionário inserido'}) #201: Created
+    @api.expect(campos_obrigatorios_para_insercao)  
+    def post(self):
+        args = parser.parse_args()
+        id = -1
+        for funcionario in Funcionarios:
+            if int(funcionario['id']) > id:
+                    id = int(funcionario['id'])
+        id = id + 1
+        funcionario = {'id': id, 'nome': args['nome'],  'horas': args['horas'], 'valor': args['valor']}
+        Funcionarios.append(funcionario)
+        return funcionario, 201, #201: Created
+    
+class ListaPagamentos(Resource):
+    def get(self):
+        pagamentos = []
+        for funcionario in Funcionarios:
+            funcionario = {'id': funcionario['id'], 'nome': funcionario['nome'],  'pagamento': funcionario['horas']*funcionario['valor']}
+            pagamentos.append(funcionario)
+        return pagamentos, 201, #201: Created
+    
+class pagamento(Resource):
+    def get(self, id):
+        aborta_se_o_funcionário_nao_existe(id)
+        pagamentos = []
+        funcionario =  Funcionarios[int(id)]
+        funcionario1 = {'id': funcionario['id'], 'nome': funcionario['nome'],  'pagamento': funcionario['horas']*funcionario['valor']}
+        pagamentos.append(funcionario1)
+        
+        return pagamentos, 
+class PagamentoMaior(Resource):
+    def get(self):
+        pagamentos = []
+        
+        for funcionario in Funcionarios:
+           
+            funcionario =  float(funcionario['horas']*funcionario['valor'])
+            pagamentos.append(funcionario)
+            
+        maior = float()
+        
+        for i in pagamentos:
+            if i > maior:
+                maior = i
+        maior= {'maior_pagamento': maior}
+        return maior
+    
+class PagamentoMenor(Resource):
+    def get(self):
+        pagamentos = []
+        
+        for funcionario in Funcionarios:
+           
+            funcionario =  float(funcionario['horas']*funcionario['valor'])
+            pagamentos.append(funcionario)
+            
+        menor = pagamentos[0]
+        
+        for i in pagamentos:
+            if menor >= i:
+                menor = i
+            
+        menor= {'menor_pagamento': menor}
+        return menor
+class PagamentoTotal(Resource):
+    def get(self):
+        pagamentos = []
+        
+        for funcionario in Funcionarios:
+           
+            funcionario =  float(funcionario['horas']*funcionario['valor'])
+            pagamentos.append(funcionario)
+            
+        total = 0
+        for i in pagamentos:
+            total = total + i
+                
+            
+        total= {'pagamento_total': total}
+        return total
+    
+##
+## Roteamento de recursos:
+##  
+api.add_resource(funcionario, '/funcionario/<id>')
+api.add_resource(ListaFuncionarios, '/funcionario')
+api.add_resource(ListaPagamentos, '/pagamento')
+api.add_resource(pagamento, '/pagamento/<id>')
+api.add_resource(PagamentoMaior, '/pagamento/maior')
+api.add_resource(PagamentoMenor, '/pagamento/menor')
+api.add_resource(PagamentoTotal, '/pagamento/total')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
